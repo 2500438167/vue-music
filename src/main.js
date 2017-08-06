3031ee6baec34391cac661168c92712c3c6c337b
if <2013goto@sina.cn>
2017-08-06 06:55:55 -0400

    将es6的转为浏览器支持的形式.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
 
 // Vue.config.productionTip = false
 
+import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
+
+// 去掉三百毫秒延时
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
