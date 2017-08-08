01dde2ca127741f1eadf33a036a5e5d595aea42a
zhangjinxi <zhangjinxi@capitalwater.cn>
2017-08-08 19:28:29 +0800

    添加lazyload的支持，并注册/使用。通过needsclick停止fastclick对事件的拦截。

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,8 @@
           <slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img @load="loadImage" :src="item.picUrl" />
+                <!-- better-scroll和fastclick点击冲突时，needsclick样式可以停止fastclick对点击事件的拦截 -->
+                <img class="needsclick" @load="loadImage" :src="item.picUrl" />
               </a>
             </div>
           </slider>
@@ -16,7 +17,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl" />
+                <img width="60" height="60" v-lazy="item.imgurl" />
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.author"></h2>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 // Vue.config.productionTip = false
 
-import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
 
 // 去掉三百毫秒延时
 fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
